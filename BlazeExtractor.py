b5f41c2a8b67b43ec42dc773f586a13f3de52d01
Gustavo Lopes <gustavolopes.dev@gmail.com>
2022-04-10 13:56:33 -0300

    implemented

--- BlazeExtractor.py
+++ BlazeExtractor.py
@@ -1,20 +1,17 @@
 #import requests
 #import pandas as pd
-from pickletools import optimize
-from threading import Thread
-from unittest import result
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 from datetime import datetime
-import Roll
 
 chrome_options = Options()
 chrome_options.add_argument('--log-level=3')
 chrome_options.headless = True
-
 
 print("Loading webpage ...")
 driver = webdriver.Chrome(executable_path="chromedriver.exe", options=chrome_options)
@@ -22,27 +19,41 @@ driver.get('https://blaze.com/pt/games/double')
 sleep(1)
  
 def get_timer():
- timer = driver.find_element(by=By.XPATH, value='//*[@id="roulette-timer"]/div/div[2]/span')
- timer_html = timer.get_attribute('outerHTML')
- soup = BeautifulSoup(timer_html, 'html.parser')
- span = soup.find("span")
- return span.text
+    timer = driver.find_element(by=By.XPATH, value='//*[@id="roulette-timer"]/div/div[2]/span')
+    timer_html = timer.get_attribute('outerHTML')
+    soup = BeautifulSoup(timer_html, 'html.parser')
+    span = soup.find("span")
+    return span.text
 
 def get_last_roll():
- timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
- file_name = "blaze-result.txt"
- print("[LOG] new result at "+file_name+" -", timestamp)
- initial_load = driver.find_element(by=By.XPATH, value='//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div')
- initial_load_html = initial_load.get_attribute('outerHTML')
- soup = BeautifulSoup(initial_load_html, 'html.parser')
- last_roll = soup.find("div", {"class": "sm-box"})
- f = open("blaze-result.txt", "a")
- color = str(last_roll['class'][1])
- number = str(last_roll.text)
- line =  color+","+ number + "\n"
- f.write(line)
- row = Roll(color, number, timestamp)
- print(row.color)
+    initial_load = driver.find_element(by=By.XPATH, value='//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div')
+    initial_load_html = initial_load.get_attribute('outerHTML')
+    soup = BeautifulSoup(initial_load_html, 'html.parser')
+    last_roll = soup.find("div", {"class": "sm-box"})
+    color = str(last_roll['class'][1])
+    number = str(last_roll.text)
+    line = color+", "+number
+    return line
 
-get_last_roll()
+def get_state_roll():
+    initial_load = driver.find_element(by=By.XPATH, value='//*[@id="roulette-timer"]/div[1]')
+    initial_load_html = initial_load.get_attribute('outerHTML')
+    soup = BeautifulSoup(initial_load_html, 'html.parser')
+    roll_state = soup.find("div", {"class": "time-left"})
+    return roll_state.text
+
+def get_timestamp():
+    now = datetime.now()
+    return now.strftime("%d/%m/%Y %H:%M:%S")
+
+while True:
+    if WebDriverWait(driver, 50).until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="roulette-timer"]/div[1]'), "Blaze Girou")):
+        print("\n")
+        print(get_state_roll())
+        print(get_timer())
+        print(get_last_roll())
+        print(get_timestamp())
+        sleep(10)    
+
+
 

48b4ddcc5572cb74e8f23e5cf2482f69bfa2c324
Gustavo Lopes <gustavolopes.dev@gmail.com>
2022-04-13 22:13:19 -0300

    implement bets extraction

--- BlazeExtractor.py
+++ BlazeExtractor.py
@@ -12,7 +12,7 @@ from selenium.common.exceptions import TimeoutException
 
 chrome_options = Options()
 chrome_options.add_argument('--log-level=3')
-chrome_options.headless = True
+chrome_options.headless = False
 
 host = "https://blaze.com/pt/games/double"
 storage_path = "storage/"
@@ -21,7 +21,7 @@ print("Starting BlazeExtractor on "+host+" ...")
 driver.get(host)
 sleep(1)
 
-def get_timer():
+def get_roulette_timer():
     timer = driver.find_element(by=By.XPATH, value='//*[@id="roulette-timer"]/div/div[2]/span')
     timer_html = timer.get_attribute('outerHTML')
     soup = BeautifulSoup(timer_html, 'html.parser')
@@ -29,9 +29,9 @@ def get_timer():
     return span.text
 
 def get_last_roll():
-    initial_load = driver.find_element(by=By.XPATH, value='//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div')
-    initial_load_html = initial_load.get_attribute('outerHTML')
-    soup = BeautifulSoup(initial_load_html, 'html.parser')
+    element = driver.find_element(by=By.XPATH, value='//*[@id="roulette-recent"]/div/div[1]/div[1]/div/div')
+    element_html = element.get_attribute('outerHTML')
+    soup = BeautifulSoup(element_html, 'html.parser')
     last_roll = soup.find("div", {"class": "sm-box"})
     color = str(last_roll['class'][1])
     number = str(last_roll.text)
@@ -39,9 +39,9 @@ def get_last_roll():
     return line
 
 def get_state_roll():
-    initial_load = driver.find_element(by=By.XPATH, value='//*[@id="roulette-timer"]/div[1]')
-    initial_load_html = initial_load.get_attribute('outerHTML')
-    soup = BeautifulSoup(initial_load_html, 'html.parser')
+    element = driver.find_element(by=By.XPATH, value='//*[@id="roulette-timer"]/div[1]')
+    element_html = element.get_attribute('outerHTML')
+    soup = BeautifulSoup(element_html, 'html.parser')
     roll_state = soup.find("div", {"class": "time-left"})
     return roll_state.text
 
@@ -55,6 +55,30 @@ def get_file_name():
     formated_now = now.strftime("%d-%m-%Y_%H.%M.%S")
     return "blaze-dataset-"+formated_now+".txt"
 
+def get_red_bet():
+    element = driver.find_element(by=By.XPATH, value='//*[@id="roulette"]/div/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
+    element_html = element.get_attribute('outerHTML')
+    soup = BeautifulSoup(element_html, 'html.parser')
+    red_bet = soup.find("span")
+    red_bet_txt = red_bet.text
+    return red_bet_txt.replace("R$", "")
+
+def get_black_bet():
+    element = driver.find_element(by=By.XPATH, value='//*[@id="roulette"]/div/div[2]/div/div[3]/div/div[3]/div[1]/div[2]')
+    element_html = element.get_attribute('outerHTML')
+    soup = BeautifulSoup(element_html, 'html.parser')
+    black_bet = soup.find("span")
+    black_bet_txt = black_bet.text
+    return black_bet_txt.replace("R$", "")
+
+def get_white_bet():
+    element = driver.find_element(by=By.XPATH, value='//*[@id="roulette"]/div/div[2]/div/div[2]/div/div[3]/div[1]/div[2]')
+    element_html = element.get_attribute('outerHTML')
+    soup = BeautifulSoup(element_html, 'html.parser')
+    white_bet = soup.find("span")
+    white_bet_txt = white_bet.text
+    return white_bet_txt.replace("R$", "")
+
 def write_to_file(file_name, row):
     file = open(storage_path+file_name, "a")
     file.write(row)
@@ -66,25 +90,31 @@ def restart():
     sleep(2)
     start()
 
-def print_data_row(state_roll, timer, last_roll, timestamp):
+def print_data_row(data_row):
     print("\n")
-    print(state_roll)
-    print(timer)
-    print(last_roll)
-    print(timestamp)
+    print(data_row)
 
 def start():
     file_name = get_file_name()
     print("BlazeExtractor is running! :)")
+
     while True:
-        try:
-            if WebDriverWait(driver, 150).until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="roulette-timer"]/div[1]'), "Blaze Girou")):
+        try:  
+            red_bet = ''
+            white_bet = ''
+            black_bet = ''
+            if WebDriverWait(driver, 60).until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="roulette-timer"]/div[1]'), "Girando...")):
+                sleep(1.50)
+                red_bet = get_red_bet()
+                white_bet = get_white_bet()
+                black_bet = get_black_bet()
+            if WebDriverWait(driver, 60).until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="roulette-timer"]/div[1]'), "Blaze Girou")):
                 timestamp = get_timestamp()
                 state_roll = get_state_roll()
-                timer = get_timer()
+                roulette_timer = get_roulette_timer()
                 last_roll = get_last_roll()
-                print_data_row(state_roll, timer, last_roll, timestamp)
-                data_row = timestamp+'\t'+state_roll+'\t'+timer+'\t'+last_roll+'\n'
+                data_row = timestamp+'\t'+state_roll+'\t'+roulette_timer+'\t'+last_roll+'\t'+red_bet+'\t'+white_bet+'\t'+black_bet+'\n'
+                print_data_row(data_row)
                 write_to_file(file_name, data_row)
                 sleep(10)   
         except TimeoutException:
